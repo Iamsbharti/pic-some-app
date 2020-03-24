d04ee8d6bfb40a0c40c965750c55374074db340e
Saurabh <saurabhbharti9@gmail.com>
2020-03-25 02:52:56 +0530

    implemented consumer to display image and fixed css

--- src/component/Image.js
+++ src/component/Image.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Image({ className, url }) {
+  const [hovered, setHovered] = useState(false);
+  console.log(hovered);
   return (
-    <div className={`${className} image-container`}>
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <img src={url} className="image-grid" alt="?" />
     </div>
   );
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { PicSomeContextConsumer } from "../picSomeContext";
-import Image from '../component/Image';
+import Image from "../component/Image";
 import { getClass } from "../utils/index";
 function Photos() {
   return (
     <main className="photos">
-      {" "}
-      <PicSomeContextConsumer>{allPhotos => {
-        allPhotos.map(photo=> )
-      }}</PicSomeContextConsumer>
+      <PicSomeContextConsumer>
+        {({ allPhotos }) =>
+          allPhotos.map((photo, index) => (
+            <Image
+              url={photo.url}
+              key={photo.index}
+              className={getClass(index)}
+            />
+          ))
+        }
+      </PicSomeContextConsumer>
     </main>
   );
 }
